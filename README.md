76af35424b459c00e1bf9ac8973691e21eb9cac1
Tushar <tusharthakuhp@gmail.com>
2024-07-08 01:06:32 +0530

    modified:   README.md modified:   index.html modified:   rps.js

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # rock-paper-scissors
+
+non-GUI rock paper scissors game made using javascript.
+
+https://tusharthakur04.github.io/rock-paper-scissors/
---
+++ rps.js
@@ -0,0 +1,76 @@
+let computerScore = 0
+let humanScore = 0 ;
+
+function getComputerChoice(){
+    let a = Math.floor(Math.random() * 3) 
+switch(a){
+    case 0 :
+        return "rock" ;
+    case 1 :
+        return "paper" ;
+    case 2 :
+        return "scissors" ;
+    }
+
+}
+
+// console.log(getComputerChoice()) ;
+
+ function getHumanChoice(){
+    let b = prompt("rock , paper or scissors").toLowerCase();
+    return b ;
+
+ }
+
+// console.log(getHumanChoice()) ;
+
+function playRound(computerChoice , humanChoice ){
+    if(computerChoice === humanChoice)
+        console.log("Draw") ;
+    else if (computerChoice==="rock"){
+        if(humanChoice==="paper"){
+            humanScore++ ;
+            console.log("You win!");
+        }
+        else{
+            computerScore++ ;
+            console.log("You lose!") ;
+        }
+    }
+        else if (computerChoice==="paper"){
+            if(humanChoice==="scissors"){
+                humanScore++ ;
+                console.log("You win!");
+            }
+            else{
+                computerScore++;
+                console.log("You lose!");
+            }
+        }
+        else if (computerChoice==="scissors"){
+            if(humanChoice==="rock"){
+                humanScore++ ;
+                console.log("You win!");
+            }
+            else{
+                computerScore++;
+                console.log("You lose!");
+            }
+        }
+
+}
+
+// playRound(getComputerChoice , getHumanChoice);
+
+function playGame(){
+    for(let i = 0 ; i < 5 ; i++){
+        let computerChoice = getComputerChoice() ;
+        let humanChoice =getHumanChoice();
+        playRound(computerChoice , humanChoice) ;
+        
+    }
+    console.log(`Final Scores: Your: ${humanScore}, Computer: ${computerScore}`);
+}
+
+
+playGame();
